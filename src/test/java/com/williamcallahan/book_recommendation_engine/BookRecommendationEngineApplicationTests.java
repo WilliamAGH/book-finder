1b58ce1eeda88ae8901240d08e69ea6409ad6062
William Callahan <william@aventure.vc>
2025-05-13 10:33:57 -0700

    test: add initial application tests

---
+++ src/test/java/com/williamcallahan/book_recommendation_engine/BookRecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamcallahan.book_recommendation_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendationEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
